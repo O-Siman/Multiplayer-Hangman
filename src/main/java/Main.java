3b969d0b704e0598f6b8c07c8af4c01052b1702b
O-Siman <72103629+O-Siman@users.noreply.github.com>
2022-01-19 12:17:22 -0500

    Server and client

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,47 @@
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        GameType gameType;
+        if (args.length > 0) {
+            if (args[0].equals("server"))
+                gameType = GameType.SERVER;
+            else if (args[0].equals("client"))
+                gameType = GameType.CLIENT;
+            else
+                return;
+        } else
+            return;
+
+        switch (gameType) {
+            case SERVER -> {
+                ServerWebSocket server = new ServerWebSocket(new InetSocketAddress(8080));
+                server.start();
+            }
+            case CLIENT -> {
+                try {
+                    runClient();
+                } catch (URISyntaxException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
+    public static void runClient() throws URISyntaxException, InterruptedException {
+        URI serverAddressUri = new URI("wss://localhost:8080");
+        ClientWebSocket client = new ClientWebSocket(serverAddressUri);
+        System.out.println("Connecting...");
+        client.connectBlocking();
+        System.out.println("Sending message");
+        client.send("Hello there");
+        System.out.println("Closing");
+        client.close();
+    }
+}
+
+enum GameType {
+    SERVER, CLIENT
 }
