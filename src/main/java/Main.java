b14dd1aa95476b7da260a692243338dc7a82eb39
O-Siman <72103629+O-Siman@users.noreply.github.com>
2022-02-23 11:15:23 -0500

    Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,9 @@ public class Main {
     public static void main(String[] args) {
         GameType gameType;
         if (args.length > 0) {
-            if (args[0].equals("server"))
-                gameType = GameType.SERVER;
-            else
-                gameType = GameType.CLIENT;
+            gameType = GameType.SERVER;
         } else
-            return;
+            gameType = GameType.CLIENT;
 
         switch (gameType) {
             case SERVER -> new Server().main();
